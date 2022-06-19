d166b23a1c7a120e84fc3cca5eda127269b12af3
Dylan Falconer <me@falconerd.com>
2022-06-19 21:45:43 +1000

    config module, write files, add time module

--- src/engine/global.h
+++ src/engine/global.h
@@ -1,13 +1,16 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
+#pragma once
 
-#include "render/render.h"
+#include "render.h"
+#include "config.h"
+#include "input.h"
+#include "time.h"
 
 typedef struct global {
-    Render_State render;
+	Render_State render;
+	Config_State config;
+	Input_State input;
+	Time_State time;
 } Global;
 
 extern Global global;
-
-#endif
 
--- src/main.c
+++ src/main.c
@@ -5,35 +5,57 @@
 #include <SDL2/SDL.h>
 
 #include "engine/global.h"
+#include "engine/config.h"
+#include "engine/input.h"
+#include "engine/time.h"
+
+static vec2 pos;
+static bool should_quit = false;
+
+static void input_handle(void) {
+	if (global.input.left == KS_PRESSED || global.input.left == KS_HELD)
+		pos[0] -= 500 * global.time.delta;
+	if (global.input.right == KS_PRESSED || global.input.right == KS_HELD)
+		pos[0] += 500 * global.time.delta;
+	if (global.input.up == KS_PRESSED || global.input.up == KS_HELD)
+		pos[1] += 500 * global.time.delta;
+	if (global.input.down == KS_PRESSED || global.input.down == KS_HELD)
+		pos[1] -= 500 * global.time.delta;
+	if (global.input.escape == KS_PRESSED || global.input.escape == KS_HELD)
+		should_quit = true;
+}
 
 int main(int argc, char *argv[]) {
-    render_init();
+	time_init(60);
+	config_init();
+	render_init();
 
-    bool should_quit = false;
+	pos[0] = global.render.width * 0.5;
+	pos[1] = global.render.height * 0.5;
 
-    while (!should_quit) {
-        SDL_Event event;
+	while (!should_quit) {
+		time_update();
 
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                should_quit = true;
-                break;
-            default:
-                break;
-            }
-        }
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				should_quit = true;
+				break;
+			}
+		}
 
-        render_begin();
+		input_update();
+		input_handle();
 
-        render_quad(
-            (vec2){global.render.width * 0.5, global.render.height * 0.5},
-            (vec2){50, 50},
-            (vec4){0, 1, 0, 1});
+		render_begin();
 
-        render_end();
-    }
+		render_quad(pos, (vec2){ 50, 50 }, (vec4){ 0, 1, 0, 1 });
 
-    return 0;
+		render_end();
+		time_update_late();
+	}
+
+	return 0;
 }
 
