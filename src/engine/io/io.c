3733a667a304e9076904a601c38dabb9b2ea266d
Dylan Falconer <me@falconerd.com>
2022-06-20 16:26:11 +1000

    add null check

--- src/engine/io/io.c
+++ src/engine/io/io.c
@@ -73,7 +73,7 @@ File io_file_read(const char *path) {
 
 int io_file_write(void *buffer, size_t size, const char *path) {
 	FILE *fp = fopen(path, "wb");
-	if (!fp)
+	if (!fp || ferror(fp))
 		ERROR_RETURN(1, "Cannot write file: %s.\n", path);
 
 	size_t chunks_written = fwrite(buffer, size, 1, fp);
